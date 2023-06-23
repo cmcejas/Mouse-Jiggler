bbc9271825a50f600cb78ac3429202f2c625cf56
Carlos <carlosmcejas@gmail.com>
2023-06-23 00:33:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ screenWidth, screenHeight = pyautogui.size()
 x, y = pyautogui.position()
 
 if(screenWidth - 1 == y and screenHeight - 1 == x):
-  pyautogui.moveTo(x - 1, y - 1)
-  time.sleep(45)
-  pyautogui.moveTo(x, y)
+  while (running == True):
+    pyautogui.moveTo(x - 1, y - 1)
+    time.sleep(45)
+    pyautogui.moveTo(x, y)
 
 else:
-  pyautogui.moveTo(x + 1, y + 1)
-  time.sleep(45)
-  pyautogui.moveTo(x, y)
+  while (running == True):
+    pyautogui.moveTo(x + 1, y + 1)
+    time.sleep(45)
+    pyautogui.moveTo(x, y)
